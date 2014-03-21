b3ac9d08d28b18df1bb7df93732b44c837840b3e
melody <874429842@qq.com>
2014-03-21 22:54:57 +0800

    3.21

--- login.py
+++ login.py
@@ -1,72 +1,119 @@
 ﻿from bottle import run, route, template, debug, request, redirect
 import sqlite3
 import static
+import smtplib
+from email.mime.text import MIMEText
+from email.header import Header
+import random
 
 
 class User:
 	def __init__(self, user_username, user_email, user_password):
 		self.user_username = user_username
 		self.user_email = user_email
-		self.user_password = user_password
-
+		self.user_password = user_password		
 		
+		
+class Password:
+	def __word__(self, user_name, password, msg):
+		self.user_name = user_name
+		self.password = password
+		self.msg = msg
+		
+		
+#登陆		
 @route('/')
 def login():
 	return template('login')
-
 	
+	
+@route("/getname")
+def getname():
+	user_name = request.GET.user_name
+	user_password = request.GET.user_password
+	con = sqlite3.connect("user.db")
+	cur = con.cursor()
+	password = cur.execute("""
+	select user_password from user 
+	where user_username = "user_name" or user_email = "user_name"
+	"""
+	)
+	if str(user_password) == str(password):
+		redirect("/main")
+	else:
+		redirect("/")
+
+#找回密码	
+@route('/retrievepassword')
+def retrievepassword():
+	return template('retrievepassword')
+		
+		
+@route('/getemail')
+def getemail():
+	user_email = request.GET.email
+	msg = request.GET.msg
+	sender = 'longlong20140320@126.com'
+	receiver = 'msg'
+	subject = 'python email test'
+	smtpserver = 'smtp.126.com'
+	username = 'longlong20140320'
+	password = 'longlong'
+
+	msgstring = "请输入后边的随机码："
+	for i in range(0,10):
+		msgstring = msgstring + str(random.randint(10,99))
+
+	msg = MIMEText(msgstring,'html','utf-8')  
+	msg['Subject'] = Header(subject, 'utf-8') 
+
+	smtp = smtplib.SMTP()
+	smtp.connect(smtpserver)
+	smtp.login(username, password)
+	smtp.sendmail(sender, receiver, msg.as_string())
+	smtp.quit()
+
+
+#注册	
 @route('/registration')
 def registration():
 	return template('registration')
 	
 	
-@route('/retrievepassword')
-def registration():
-	password=getpassword()
-	return template('retrievepassword',user_password=password)
-
-
-@route('/main')
-def userinfo():
-	return template('main')
-
-	
-@route('/userinfo')
-def userinfo():
-	return template('userinfo')
-	
-	
-@route('/modifypassword')
-def modifypassword():
-	return template('modifypassword')
-
-	
-@route('/getpassword')
-def getpassword():
-	username = request.GET.username
-	email = request.GET.email
+@route("/user")
+def user():
 	con = sqlite3.connect("user.db")
 	cur = con.cursor()
-	sql = """
-	select user_password from user where user_username="name" and user_email="email"
-	"""
-	cur.execute(sql)
+	cur.execute( """create table if not exists user(
+	user_userid integer primary key,
+	user_username text unique,
+	user_nickname text,
+	user_email text,
+	user_password text
+	)"""
+	)
+	con = sqlite3.connect("userid_msg.db")
+	cur.execute( """create table if not exists userid_msg(
+	user_userid integer primary key,
+	user_username text,
+	user_msg text,
+	foreign key(user_userid) references user(user_userid)
+	)"""
+	)
 	con.commit()
-	
-	
-@route("/initdb")
-def initdb():
-		con = sqlite3.connect("user.db")
-		cur = con.cursor()
-		sql = """create table if not exists user(
-		user_userid integer primary key,
-		user_username text,
-		user_email text,
-		user_password text
-		)"""
-		cur.execute(sql)
-		con.commit()
-		redirect('/registration')
+	redirect('/registration')
+def project():
+	con = sqlite3.connect("project.db")
+	cur = con.cursor()
+	cur.execute( """create table if not exists project(
+	project_id integer primary key,
+	project_name text unique,
+	project_dscrb text,
+	user_userid integer,
+	project_nature integer,
+	foreign key(user_userid) references user(user_userid)
+	)"""
+	)
 	
 	
 def insertdb(userobj):
@@ -89,7 +136,36 @@ def getdata():
 	insertdb(user)
 	redirect("/")
 	
+
+
+
+#主页面	
+@route('/main')
+def main():
+	return template('main')
+
 	
+@route('/userinfo')
+def userinfo():
+	return template('userinfo')
+	
+	
+@route('/modifypassword')
+def modifypassword():
+	return template('modifypassword')
+
+	
+
+	
+	
+
+#用户信息表		
+@route("/userdb")
+def userdb():
+	results = selectdb()
+	return template("user", userlist = results)
+
+		
 def selectdb():
 	con = sqlite3.connect("user.db")
 	cur = con.cursor()
@@ -101,10 +177,7 @@ def selectdb():
 	return results
 	
 		
-@route("/showdb")
-def showdb():
-	results = selectdb()
-	return template("show", userlist = results)
+
 	
 	
 debug(True)  
